a3027b2f4308e0b38a579e8e825df9cc54aad6e0
mlormois <ocae.lormois@gmail.com>
2023-12-05 08:23:56 +0100

    installation de swagger

--- back/src/main.ts
+++ back/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.BACK_PORT);
+    const app = await NestFactory.create(AppModule);
+
+    // Pre-Configuration Swagger
+    const config = new DocumentBuilder()
+    .setTitle('Nom de votre API')
+    .setDescription('Description de votre API')
+    .setVersion('1.0')
+    .addTag('Exemple')
+    .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('swagger', app, document);
+    // ---
+
+    await app.listen(process.env.BACK_PORT);
 }
 bootstrap();
