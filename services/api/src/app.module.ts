103acbbd08eaa8f6d02d821d53b41bb9dbe96720
mlormois <mlormois@student.42.fr>
2024-02-12 19:11:19 +0100

    better prisma schema with ep api

--- services/api/src/app.module.ts
+++ services/api/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserService } from './users/users.service';
+import { PostService } from './posts/posts.service';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        ConfigModule.forRoot({
+			isGlobal: true,
+		}),
+        PrismaModule
+    ],
+    controllers: [AppController],
+    providers: [AppService, UserService, PostService],
 })
 export class AppModule {}
